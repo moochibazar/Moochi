5fec4a887b7375cb319648f557ad8dca41eb443b
Moochi <moochibazar@gmail.com>
2026-07-28 16:50:03 +0330

    Create app.js

---
+++ js/app.js
@@ -0,0 +1,21 @@
+// وقتی صفحه باز شد
+document.addEventListener("DOMContentLoaded", () => {
+
+    const adminBtn = document.getElementById("adminBtn");
+    const customerBtn = document.getElementById("customerBtn");
+
+    // ورود مدیر
+    if (adminBtn) {
+        adminBtn.addEventListener("click", () => {
+            window.location.href = "admin.html";
+        });
+    }
+
+    // ورود مشتری
+    if (customerBtn) {
+        customerBtn.addEventListener("click", () => {
+            window.location.href = "customer.html";
+        });
+    }
+
+});
