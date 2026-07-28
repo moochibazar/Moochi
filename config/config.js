11eb8d34669d79fc685adb1266fe09b2651194b3
Moochi <moochibazar@gmail.com>
2026-07-28 17:40:03 +0330

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,7 +1,7 @@
 const CONFIG = {
 
     // آدرس Google Apps Script
-    WEB_APP_URL: "",
+    WEB_APP_URL: "https://script.google.com/macros/s/AKfycbyTyAFO3dMa05f-APh4BOWWhbfxD-6cnnHdTTI2PvVR_gBSblJNKsHQrJ6DisazF70U/exec",
 
     // رمز ورود مدیر
     ADMIN_PASSWORD: "123456",
