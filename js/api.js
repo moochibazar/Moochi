1ee1842af8e75b7d3dceb7f74d1e01f88e76cdd2
Moochi <moochibazar@gmail.com>
2026-07-28 18:05:15 +0330

    Update api.js

--- js/api.js
+++ js/api.js
@@ -1,1 +1,24 @@
+async function apiRequest(action, data = {}) {
 
+    const response = await fetch(CONFIG.WEB_APP_URL, {
+
+        method: "POST",
+
+        headers: {
+            "Content-Type": "application/json"
+        },
+
+        body: JSON.stringify({
+
+            action,
+
+            ...data
+
+        })
+
+    });
+
+
+    return await response.json();
+
+}
