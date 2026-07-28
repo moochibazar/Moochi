b47d8548fad837f016739755cfea408e7f2a5e79
Moochi <moochibazar@gmail.com>
2026-07-28 16:49:24 +0330

    Create config.js

---
+++ config/config.js
@@ -0,0 +1,15 @@
+const CONFIG = {
+
+    // آدرس Google Apps Script
+    WEB_APP_URL: "",
+
+    // رمز ورود مدیر
+    ADMIN_PASSWORD: "123456",
+
+    // نام پروژه
+    APP_NAME: "سیستم حسابداری موچی",
+
+    // نسخه برنامه
+    VERSION: "1.0.0"
+
+};
