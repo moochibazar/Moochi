866a496d375bd69de6c3fc02d13054bc129833ed
Moochi <moochibazar@gmail.com>
2026-08-03 10:11:57 +0330

    Update adminMenu.js

--- js/adminMenu.js
+++ js/adminMenu.js
@@ -1,1 +1,27 @@
+window.showAdminMenu = function () {
 
+    const pageContent = document.getElementById("pageContent");
+
+    pageContent.innerHTML = `
+
+    <h2>⚙️ ادمین</h2>
+
+    <div class="card" onclick="showOrdersPage()">
+
+        <h3>🍡 ثبت سفارش</h3>
+
+        <p>ثبت سفارش جدید</p>
+
+    </div>
+
+    <div class="card" onclick="showPaymentsPage()">
+
+        <h3>💰 پرداخت جدید</h3>
+
+        <p>ثبت پرداخت مشتری</p>
+
+    </div>
+
+    `;
+
+};
