31d5756d5d7b55a5cfdb5bc7ad80b5e7b097e19e
Moochi <moochibazar@gmail.com>
2026-08-03 10:32:54 +0330

    Update admin-panel.js

--- js/admin-panel.js
+++ js/admin-panel.js
@@ -3,21 +3,15 @@ document.addEventListener("DOMContentLoaded", () => {
     const pageContent = document.getElementById("pageContent");
 
     pageContent.innerHTML = `
-
         <h2>⚙️ پنل ادمین</h2>
 
-        <div class="card">
-            <button onclick="location.href='admin.html#orders'">
-                🍡 ثبت سفارش
-            </button>
+        <div class="card" onclick="location.href='admin.html?page=orders'">
+            <h3>🍡 ثبت سفارش</h3>
         </div>
 
-        <div class="card">
-            <button onclick="location.href='admin.html#payments'">
-                💰 پرداخت جدید
-            </button>
+        <div class="card" onclick="location.href='admin.html?page=payments'">
+            <h3>💰 پرداخت جدید</h3>
         </div>
-
     `;
 
 });
